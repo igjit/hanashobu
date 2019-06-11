ce3e963c12765aa5d7c80d0189a9f0eea4cb60d7
igjit <igjit1@gmail.com>
2019-06-11 09:38:00 +0900

    Fix coding style

--- plot.R
+++ plot.R
@@ -7,7 +7,7 @@ out <- "plot.png"
 df <- read_csv(csv, col_names = c("md", "n"))
 df %<>% mutate(date = as.Date(md, format = "%m月%d日"))
 
-if(Sys.info()['sysname'] == "Darwin") theme_set(theme_grey(base_family = "HiraKakuProN-W3"))
+if (Sys.info()["sysname"] == "Darwin") theme_set(theme_grey(base_family = "HiraKakuProN-W3"))
 
 p <- ggplot(df, aes(x = date, y = n)) +
   geom_line() +
